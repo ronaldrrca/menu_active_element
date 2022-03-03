0b4f81dd70ae089ada1c06ea569eb7efa8930bd7
Ronald Rodriguez Campillo <ronaldrrca@gmail.com>
2022-03-03 17:52:13 -0500

    Reduciendo el código JS

--- code.js
+++ code.js
@@ -1,8 +1,3 @@
-const item_index = document.getElementById("item_index");
-const item_productos = document.getElementById("item_productos");
-const item_servicios = document.getElementById("item_servicios");
-const item_contactame = document.getElementById("item_contactame");
-
 let url = window.location.href;
 let from = url.lastIndexOf("/") + 1;
 let to = url.indexOf(".html"); 
@@ -10,19 +5,19 @@ let pageName = url.slice(from, to);
 
 switch (pageName) {
     case "index":
-        item_index.style.color = "#FF6600";
+        document.getElementById("item_index").style.color = "#FF6600";
         break;
     
     case "contactame":
-        item_contactame.style.color = "#FF6600";
+        document.getElementById("item_contactame").style.color = "#FF6600";
         break;
 
     case "productos":
-        item_productos.style.color = "#FF6600";
+        document.getElementById("item_productos").style.color = "#FF6600";
         break;
 
     case "servicios":
-        item_servicios.style.color = "#FF6600";
+        document.getElementById("item_servicios").style.color = "#FF6600";
         break;
 }
 
