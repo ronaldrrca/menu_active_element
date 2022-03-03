6a213184b1a6fc45e7eac01ae35b342570af9a22
Ronald Rodriguez Campillo <ronaldrrca@gmail.com>
2022-03-03 17:02:05 -0500

    Código funcional para lo que se quiere mostrar

--- code.js
+++ code.js
@@ -1,1 +1,28 @@
+const item_index = document.getElementById("item_index");
+const item_productos = document.getElementById("item_productos");
+const item_servicios = document.getElementById("item_servicios");
+const item_contactame = document.getElementById("item_contactame");
+
+let url = window.location.href;
+let from = url.lastIndexOf("/") + 1;
+let to = url.indexOf(".html"); 
+let pageName = url.slice(from, to);
+
+switch (pageName) {
+    case "index":
+        item_index.style.color = "#FF6600";
+        break;
+    
+    case "contactame":
+        item_contactame.style.color = "#FF6600";
+        break;
+
+    case "productos":
+        item_productos.style.color = "#FF6600";
+        break;
+
+    case "servicios":
+        item_servicios.style.color = "#FF6600";
+        break;
+}
 
